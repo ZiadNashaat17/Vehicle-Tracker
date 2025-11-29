1f147a7ef03791367801337dfa3763d45b5d7354
Ziad Nashaat <212103116@o6u.edu.eg>
2025-11-29 22:00:34 +0200

    refactor: Simplify user routes by consolidating controller imports

--- api-gateway/src/routes/userRoutes.js
+++ api-gateway/src/routes/userRoutes.js
@@ -1,29 +1,19 @@
 import { Router } from 'express';
-import {
-  changePassword,
-  deactivateUser,
-  forgotPassword,
-  getUser,
-  login,
-  reactivateUser,
-  register,
-  resetPassword,
-  updateUser,
-  verifyEmail,
-} from '../controllers/userController.js';
+import * as userController from '../controllers/userController.js';
 import authenticate from '../middlewares/authenticate.js';
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.get('/verify-email/:token', verifyEmail);
-router.get('/get-user', authenticate, getUser);
-router.patch('/update-user', authenticate, updateUser);
-router.patch('/change-password', authenticate, changePassword);
-router.post('/forgot-password', forgotPassword);
-router.patch('/reset-password/:token', resetPassword);
-router.patch('/deactivate-user', authenticate, deactivateUser);
-router.patch('/reactivate-user', reactivateUser);
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.get('/verify-email/:token', userController.verifyEmail);
+router.post('/forgot-password', userController.forgotPassword);
+router.patch('/reset-password/:token', userController.resetPassword);
+router.patch('/reactivate-user', userController.reactivateUser);
+router.use(authenticate);
+router.get('/get-user', userController.getUser);
+router.patch('/update-user', userController.updateUser);
+router.patch('/change-password', userController.changePassword);
+router.patch('/deactivate-user', userController.deactivateUser);
 
 export default router;

ed6c79c8f07a2e106ef24e3d6535675a410e7898
Ziad Nashaat <212103116@o6u.edu.eg>
2025-12-05 20:34:15 +0200

    refactor: restructure project architecture; move entry points to src directory and implement graceful shutdown for services

--- user/src/app.js
+++ user/src/app.js
@@ -2,13 +2,18 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
+import cors from 'cors';
 import { config } from 'dotenv';
 
 config({ path: './config.env' });
 
-import globalErrorHandler from './src/middlewares/errorController.js';
-import AppError from './src/util/appError.js';
-import trackRouter from './src/routes/trackRoutes.js';
+import AppError from './util/appError.js';
+import globalErrorHandler from './middlewares/errorController.js';
+import userRouter from './routes/userRoutes.js';
+import deviceRouter from './routes/deviceRoutes.js';
+import vehicleRouter from './routes/vehicleRoutes.js';
+import geofenceRouter from './routes/geofenceRoutes.js';
+import liveRouter from './routes/liveRoutes.js';
 
 const app = express();
 const limit = rateLimit({
@@ -20,14 +25,19 @@ const limit = rateLimit({
 app.use(express.json());
 app.use(helmet());
 app.use('/api', limit);
+app.use(cors());
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV.trim() === 'development') {
   app.use(morgan('dev'));
 }
 
 app.disable('x-powered-by');
 
-app.use('/api/track', trackRouter);
+app.use('/api/user', userRouter);
+app.use('/api/vehicle', vehicleRouter);
+app.use('/api/device', deviceRouter);
+app.use('/api/geofence', geofenceRouter);
+app.use('/api/live', liveRouter);
 
 app.use((req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));

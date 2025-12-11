72f7973fc9559bcbf03ee052d381f5a6e7c8d91c
Ziad Nashaat <ziadnashaat03@gmail.com>
2025-12-11 15:22:56 +0200

    Refactor code style for consistency and readability across middleware and route files.

--- api-gateway/src/routes/consumerRoutes.js
+++ api-gateway/src/routes/consumerRoutes.js
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-import { getDeviceHistory } from '../controllers/consumerController.js';
-import authenticate from '../middlewares/authenticate.js';
+import { Router } from "express";
+import { getDeviceHistory } from "../controllers/consumerController.js";
+import authenticate from "../middlewares/authenticate.js";
 
 const router = Router();
 
-router.get('/:deviceId/history', authenticate, getDeviceHistory);
+router.get("/:deviceId/history", authenticate, getDeviceHistory);
 
 export default router;
--- api-gateway/src/routes/publisherRoutes.js
+++ api-gateway/src/routes/publisherRoutes.js
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-import { trackController } from '../controllers/publisherController.js';
-import authenticate from '../middlewares/authenticate.js';
+import { Router } from "express";
+import { trackController } from "../controllers/publisherController.js";
+import authenticate from "../middlewares/authenticate.js";
 
 const router = Router();
 
-router.post('/', authenticate, trackController);
+router.post("/", authenticate, trackController);
 
 export default router;
